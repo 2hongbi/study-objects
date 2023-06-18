a6c4926418ecfccc3f8bb98a7a1064be4e905edb
2hongbi <foxgirin950@gmail.com>
2023-06-18 18:18:07 +0900

    feat: Bag 클래스 추가

---
+++ src/ch1/Bag.java
@@ -0,0 +1,23 @@
+package ch1;
+
+public class Bag {
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
