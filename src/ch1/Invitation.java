da4849ee257795b2c272ea815a5362969f9016e9
2hongbi <foxgirin950@gmail.com>
2023-06-18 18:13:33 +0900

    feat: Invitation 과 Ticket 클래스 추가

---
+++ src/ch1/Invitation.java
@@ -0,0 +1,7 @@
+package ch1;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/ch1/Ticket.java
@@ -0,0 +1,9 @@
+package ch1;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
